ced25eb0e593b597e181f30cbf2512e386c6caf6
jprodriguez33 <johnpaul334@icloud.com>
2023-01-29 17:14:53 -0500

    Fixes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
 import java.util.Arrays;
-public class main
-{
-  public void quickSort(int arr[], int begin, int end){
+public class Main{
 
+  public static void main(String[] args){
+
+    int[] nums = {3,5,2,6,8,4,4,6,4,4,3};
+    int p1 = 0, p2 = nums.length - 1, index = 0;
+    int temp;
+
+
+    while (index <= p2) {
+      if (nums[index] < 5) {
+        temp = nums[index];
+        nums[index] = nums[p1];
+        nums[p1]= temp;
+        p1++;
+      }
+      if (nums[index] > 5) {
+        temp = nums[index];
+        nums[index] = nums[p2];
+        nums[p2] = temp;
+        p2--;
+        index--;
+      }
+      index++;
+    }
+    System.out.println(Arrays.toString(nums));
   }
 }
+
+
 
--- src/WithArray.java
+++ src/WithArray.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class WithArray {
-}
+import java.util.Arrays;
+public class WithArray {
+    public static void main(String[] args) {
+        int[] nums = {3, 5, 2, 6, 8, 4, 4, 6, 4, 4, 3};
+        int x = 5;
+        int[] array = reorder(nums, x);
+        System.out.println(Arrays.toString(array));
+
+    }
+        public static int[] reorder( int[] arr, int index) {
+            int[] array = new int[arr.length];
+            int left = 0;
+            int right = arr.length - 1;
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < index) {
+                    array[left++] = arr[i];
+                }
+                else if (arr[i] > index) {
+                    array[right--] = arr[i];
+                }
+            }
+            while (left <= right)
+                array[left++] = index;
+
+            return array;
+        }
+
+    }
+
+
+
